df8f42efd7c0ecbd89b2ecc44f9a3c2f5628a3d5
sam.gofc <smahabole@google.com>
2017-11-09 15:57:52 +0530

    Add a class decorator attempt.

---
+++ python/learning_decorators.py
@@ -0,0 +1,40 @@
+"""Experimenting with decorators."""
+
+import datetime
+# import timeit
+
+
+def time_it(func):
+  def new_func(*args, **kwargs):
+    before = datetime.datetime.now()
+    result = func(*args, **kwargs)
+    after = datetime.datetime.now()
+    print 'Time taken for {0}: {1}'.format(func.func_name, after - before)
+    print 'Arguments: {0}'.format(args)
+    print 'Keyword arguments: {0}'.format(kwargs)
+    return result
+  return new_func
+
+
+def class_timer(cls):
+  """Decorator for timing class methods."""
+
+  class NewClass(object):  # Subclass cls?
+    def __init__(self, *args, **kwargs):
+      self.original_obj = cls(*args, **kwargs)
+      self.original_cls = cls
+
+    def __getattribute__(self, attr):
+      # Not to mess up lookups for this class.
+      # Sure this won't trigger recursion?
+      # if hasattr(super(NewClass, self).__getattribute__(attr)
+      if hasattr(super(NewClass, self), attr):
+        return super(NewClass, self).__getattribute__(attr)
+
+      target = self.original_obj.__getattribute__(attr)
+      if type(target) == type(self.__init__):
+        return time_it(target)
+      return target
+
+  return NewClass
+
