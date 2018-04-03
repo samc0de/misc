1748ddd971881cbd9c073934854ca4accbd940d6
sam.gofc <smahabole@google.com>
2018-04-03 12:32:26 +0530

    Fixed missing returns to fix call heirarchy. Fixed a confusion regarding super call in __new__. Also added missing *arguments, and **kwargs.

--- python/metaclass__method_call_order.py
+++ python/metaclass__method_call_order.py
@@ -20,10 +20,14 @@ class MetaClass(type):
     print('Inside __call__ method of {0}'.format(self.__class__.__name__))
     print('Positional arguments are: %s.' % (arguments or None))
     print('Keyword arguments are: %s.' % (kwargs or None))
+    # See if popping/modifying kwargs here affects other class's methods!
+    # IT DOES! Do not pop/modify kwargs or even arguments.
+    # if kwargs.pop(DEBUG_KEY):
     if kwargs.get(DEBUG_KEY):
       set_trace()
+      # kwargs.pop(DEBUG_KEY)
 
-    return super(MetaClass, self).__call__(self)
+    return super(MetaClass, self).__call__(*arguments, **kwargs)
 
 
 class MyClass(object):
@@ -37,13 +41,21 @@ class MyClass(object):
     if kwargs.get(DEBUG_KEY):
       set_trace()
 
+    # Notice a different syntax for the super call? __new__ is a static method,
+    # so there is no binding to 'cls' in the __new__ returned by super(). It
+    # needs to be passed explicitly.
+    # https://stackoverflow.com/a/7471469/860421
+    return super(MyClass, cls).__new__(cls, *arguments, **kwargs)
+
   def __init__(self, *arguments, **kwargs):
     """Called at object (values) instantiation time of an object."""
-    print('Inside __self__ of {0}.'.format(self.__class__.__name__))
+    print('Inside __init__ of {0}.'.format(self.__class__.__name__))
     print('Positional arguments are: %s.' % (arguments or None))
     print('Keyword arguments are: %s.' % (kwargs or None))
     if kwargs.get(DEBUG_KEY):
       set_trace()
+
+    return super(MyClass, self).__init__(*arguments, **kwargs)
 
 
 if __name__ == '__main__':
@@ -51,8 +63,12 @@ if __name__ == '__main__':
   from pdb import set_trace
   parser = argparse.ArgumentParser(prog='Magic methods call order demo.')
   # TODO: Add more args which can be passed in further.
-  parser.add_argument('--' + DEBUG_KEY, action='store_false')
+  # Value store_true holds true when option is included. Opposite behaviour
+  # with store_false.
+  parser.add_argument('--' + DEBUG_KEY, action='store_true')
   args = parser.parse_args()
   # if args.get(DEBUG_KEY):
   debug = getattr(args, DEBUG_KEY)
+  if debug:
+    set_trace()
   my_object = MyClass(debug=debug)
