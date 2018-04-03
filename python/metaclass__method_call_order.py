d25fa8de6709fe73681113c8e0397d4b6efee27b
sam.gofc <smahabole@google.com>
2018-04-03 11:59:30 +0530

    Rename args to arguments to avoid confusion in pdb debugger.

--- python/metaclass__method_call_order.py
+++ python/metaclass__method_call_order.py
@@ -15,11 +15,11 @@ class MetaClass(type):
 
   Magic method of interest in this context is __call__.
   """
-  def __call__(self, *args, **kwargs):
+  def __call__(self, *arguments, **kwargs):
     """This method gets called when a new object of this class is created."""
     print('Inside __call__ method of {0}'.format(self.__class__.__name__))
-    print('Positional args are: %s.' % (args or None))
-    print('Keyword args are: %s.' % (kwargs or None))
+    print('Positional arguments are: %s.' % (arguments or None))
+    print('Keyword arguments are: %s.' % (kwargs or None))
     if kwargs.get(DEBUG_KEY):
       set_trace()
 
@@ -29,19 +29,19 @@ class MetaClass(type):
 class MyClass(object):
   __metaclass__ = MetaClass
 
-  def __new__(cls, *args, **kwargs):
+  def __new__(cls, *arguments, **kwargs):
     """Called at creation time of a new object."""
     print('Inside __new__ of {0}.'.format(cls.__name__))
-    print('Positional args are: %s.' % (args or None))
-    print('Keyword args are: %s.' % (kwargs or None))
+    print('Positional arguments are: %s.' % (arguments or None))
+    print('Keyword arguments are: %s.' % (kwargs or None))
     if kwargs.get(DEBUG_KEY):
       set_trace()
 
-  def __init__(self, *args, **kwargs):
+  def __init__(self, *arguments, **kwargs):
     """Called at object (values) instantiation time of an object."""
     print('Inside __self__ of {0}.'.format(self.__class__.__name__))
-    print('Positional args are: %s.' % (args or None))
-    print('Keyword args are: %s.' % (kwargs or None))
+    print('Positional arguments are: %s.' % (arguments or None))
+    print('Keyword arguments are: %s.' % (kwargs or None))
     if kwargs.get(DEBUG_KEY):
       set_trace()
 
