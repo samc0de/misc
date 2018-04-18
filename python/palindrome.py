d4b955083e36b20785f657d8c1d457d41d66b545
sam.gofc <smahabole@google.com>
2018-04-18 12:42:42 +0530

    String matching version doesn't work, added a note.

--- python/palindrome.py
+++ python/palindrome.py
@@ -14,10 +14,12 @@ def palindrome(string):
 
 def palindrome_seq_matcher(string):
   length = len(string)
+  # This does not work for all the cases!!!
   return string[:length / 2 + 1] == string[length:(length+1) / 2 - 2:-1]
 
 
 # Interestingly both show pretty much the same efficiency.
 # The first approach uses short circuiting, so it seems that the string matching
 # uses it as well.
+# The string matching version doesn't work! Fails for 'a' and 'aa'.
 
