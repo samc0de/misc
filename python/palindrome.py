888bbb6f4f513ca4b5849b154b7badb1ee0253cb
sam.gofc <smahabole@google.com>
2018-04-18 12:12:55 +0530

    See differences in two ways of checking if a string is palindrome.
    One way works at a lower level with indexes, while another uses string
    indexing for reversing slice of input string and then matching with
    first half. Seems that the latter uses short circuit, as the first one
    does.

---
+++ python/palindrome.py
@@ -0,0 +1,21 @@
+"""Check differences in different algos for checking if a string is palindrome.
+
+Mainly used %timeit from ipython shell for this.
+"""
+
+
+def palindrome(string):
+  """Loop approach."""
+  for idx in range(len(string) / 2):
+    if string[idx] != string[-idx - 1]:
+      return False
+  return True
+
+
+def palindrome_seq_matcher(string):
+  length = len(string)
+  return string[:length / 2 + 1] == string[length:(length+1) / 2 - 2:-1]
+
+
+# Interestingly both show pretty much the same efficiency.
+
