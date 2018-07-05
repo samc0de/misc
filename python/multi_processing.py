96fd919d10b47997e11607bfaaf7c8246cb5e777
sam_mac <sameer.mahabole@gmail.com>
2018-07-05 23:33:55 +0530

    Working basic example of multiprocessing module usage.

---
+++ python/multi_processing.py
@@ -0,0 +1,36 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import sys
+import time
+import multiprocessing
+import click
+
+
+class MyProcess(multiprocessing.Process):
+    def __init__(self, proc_id, delay=1):
+        self.id = proc_id
+        self.delay = delay
+        return super(MyProcess, self).__init__()
+
+    def run(self):
+        # Use click.echo.
+        print('Process id: %s started...' % self.id)
+        time.sleep(self.delay)
+        print('Completed run of id: %s' % self.id)
+
+
+
+@click.command(name='process command')
+@click.option('--delay', default=1, help='Process delay.')
+@click.argument('limit', type=int)
+def process_looper(limit, delay):
+    for num in range(limit):
+        proc = MyProcess(num, delay)
+        proc.start()
+
+
+if __name__ == '__main__':
+    process_looper()
