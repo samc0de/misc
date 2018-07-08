19236ce7f30d82514de7afd3b29e1e91a27a83ff
sam_mac <sameer.mahabole@gmail.com>
2018-07-08 23:33:49 +0530

    Add basic idea as a module.

---
+++ python/concurrent_image_processing.py
@@ -0,0 +1,7 @@
+# 1. Organise files in dirs.
+# 2. Image comparator to start with. Take a dir as i/p.
+# 3. Concurrency in this prob. Reading files from disk ans processing arrays
+# can be run concurretly and both are time taking.
+# 4. Start writing such concurrent functions. Profile them.
+# 5. Determine # of procs and tasks given to each. Maybe a NN task? Test on dir
+# with large # of images.
