d1c6b6dd6546343de7db4444856c72bcb0abc118
sam <sssam.sam@gmail.com>
2020-06-14 04:27:20 +0530

    TDD: Project Euler p1, cleanup.

--- python/project_euler/p1/test_solution.py
+++ python/project_euler/p1/test_solution.py
@@ -1,42 +1,41 @@
-import pytest
-
 import solution
 
 
-def test_get_multiples__divisibility():
-    """Test if the nums in the list returned are all divisible by 3 & 5."""
-    roots = (3, 5)
-    multiples = solution.get_multiples(root_factors=roots, limit=1000)
-    # assert (all(not(x % f) for f in roots for x in multiples))
-    assert (all(any(not(x % f) for f in roots) for x in multiples))
+class TestGetMultiples(object):
+
+    def test_divisibility(self):
+        """Test if the nums in the list returned are all divisible by 3 & 5."""
+        roots = (3, 5)
+        multiples = solution.get_multiples(root_factors=roots, limit=1000)
+        # assert (all(not(x % f) for f in roots for x in multiples))
+        assert (all(any(not(x % f) for f in roots) for x in multiples))
 
 
-def test_get_multiples__non_empty():
-    """Test if the nums in the list returned are all divisible by 3 & 5."""
-    roots = (3, 5)
-    multiples = solution.get_multiples(root_factors=roots, limit=1000)
-    assert len(multiples)
+    def test_non_empty(self):
+        """Test if the nums in the list returned are all divisible by 3 & 5."""
+        roots = (3, 5)
+        multiples = solution.get_multiples(root_factors=roots, limit=1000)
+        assert len(multiples)
 
 
-def test_get_multiples__limit():
-    """Test if the nums in the list returned are all divisible by 3 & 5."""
-    roots = (3, 5)
-    multiples = solution.get_multiples(root_factors=roots, limit=1000)
-    assert (all(0 < x < 1000 for x in multiples))
+    def test_limit(self):
+        """Test if the nums in the list returned are all divisible by 3 & 5."""
+        roots = (3, 5)
+        multiples = solution.get_multiples(root_factors=roots, limit=1000)
+        assert (all(0 < x < 1000 for x in multiples))
 
 
-def test_get_sum_of_devisors__int_type():
-    """Test if the nums in the list returned are all divisible by 3 & 5."""
-    roots = (3, 5)
-    total = solution.get_sum_of_devisors(root_factors=roots, limit=1000)
-    assert isinstance(total, int)
+class TestGetSum(object):
+
+    def test_int_type(self):
+        """Test if the nums in the list returned are all divisible by 3 & 5."""
+        roots = (3, 5)
+        total = solution.get_sum_of_devisors(root_factors=roots, limit=1000)
+        assert isinstance(total, int)
 
 
-def test_get_sum_of_devisors__sum_value():
-    """Test if the nums in the list returned are all divisible by 3 & 5."""
-    roots = (3, 5)
-    total = solution.get_sum_of_devisors(root_factors=roots, limit=1000)
-    assert total == 233168
-
-
-
+    def test_sum_value(self):
+        """Test if the nums in the list returned are all divisible by 3 & 5."""
+        roots = (3, 5)
+        total = solution.get_sum_of_devisors(root_factors=roots, limit=1000)
+        assert total == 233168
